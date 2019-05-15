9a94004c1d57b1678b0a2aef5e466ca5fa72fe1d
blenz <bbluegga@yahoo.com>
2019-05-14 21:51:42 -0700

    add global config for api

--- Client/src/app/_services/auth.service.ts
+++ Client/src/app/_services/auth.service.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from "rxjs/operators";
+import { map } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(model: any) {
-    return this.http.post('http://localhost:5000/api/auth/login', model)
-      .pipe(
-        map((res: any) => {
-          const user = res;
-          if (user) {
-            localStorage.setItem('token', user.token);
-          }
-        })
-      );
+    return this.http.post(environment.api + '/auth/login', model).pipe(
+      map((res: any) => {
+        const user = res;
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })
+    );
   }
 
   register(model: any) {
-    return this.http.post('http://localhost:5000/api/auth/register', model);
+    return this.http.post(environment.api + 'auth/register', model);
   }
-
 }
