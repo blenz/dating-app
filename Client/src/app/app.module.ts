3c27240e3247f4444c24660a3a9c2609e51bc2a0
blenz <bbluegga@yahoo.com>
2019-05-27 14:21:03 -0700

    add tabset to app

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
@@ -44,6 +44,7 @@ export function tokenGetter() {
     FormsModule,
     BsDropdownModule.forRoot(),
     RouterModule.forRoot(routes),
+    TabsModule.forRoot(),
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
