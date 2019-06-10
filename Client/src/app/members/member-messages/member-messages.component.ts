531b3830a1f2285220ca803bfce11fff6f42bda3
blenz <bbluegga@yahoo.com>
2019-06-09 22:54:51 -0700

    add catch for whitespace

--- Client/src/app/members/member-messages/member-messages.component.ts
+++ Client/src/app/members/member-messages/member-messages.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { Message } from 'src/app/_models/message';
 import { tap } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-messages',
@@ -11,6 +12,7 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm: NgForm;
   @Input() recipientId: number;
   messages: Message[];
   newMessage: any = {};
@@ -49,6 +51,10 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   sendMessage() {
+    if (this.newMessage.content.trim().length === 0) {
+      this.messageForm.resetForm();
+      return;
+    }
     this.newMessage.recipientId = this.recipientId;
     this.userService
       .sendMessage(this.authService.decodedToken.nameid, this.newMessage)
